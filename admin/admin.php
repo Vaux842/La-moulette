61fdfd306a2ee6787bb6a23edf1ccb08a9200d46
Vaux842 <vaux2310@gmail.com>
2020-08-14 22:14:28 +0200

    boisson

--- admin/admin.php
+++ admin/admin.php
@@ -7,7 +7,16 @@ $p = $db->query("SELECT * FROM `plat`");
 $pe = $db->query("SELECT * FROM `plat_enfant`");
 $f = $db->query("SELECT * FROM `fromage`");
 $d = $db->query("SELECT * FROM `dessert`");
-$b = $db->query("SELECT * FROM `boisson`");
+$b_B = $db->query("SELECT * FROM `boisson` where type='B'");
+$b_BSA = $db->query("SELECT * FROM `boisson` where type='BSA'");
+$b_VB = $db->query("SELECT * FROM `boisson` where type='VB'");
+$b_VRos = $db->query("SELECT * FROM `boisson` where type='VRos'");
+$b_VRou = $db->query("SELECT * FROM `boisson` where type='VRou'");
+$b_VP = $db->query("SELECT * FROM `boisson` where type='Vp'");
+$b_Ci = $db->query("SELECT * FROM `boisson` where type='Ci'");
+$b_Ch = $db->query("SELECT * FROM `boisson` where type='Ch'");
+$b_AA = $db->query("SELECT * FROM `boisson` where type='AA'");
+$b_SEBSA = $db->query("SELECT * FROM `boisson` where type='SEBSA'");
 
 ?>
 
@@ -26,13 +35,37 @@ $b = $db->query("SELECT * FROM `boisson`");
 			<h2 id="pop-up-title">Ajouter une recette :</h2>
 			<form method="POST" action="edit_recipe.php" enctype="multipart/form-data">
 				<input type="hidden" id="table_input" name="table_input" >
-				<input type="hidden" id="base-recipe-name" name="base-recipe-name" disabled>
-				<input type="hidden" id="base-recipe-image" name="base-recipe-image" disabled>
-				<input type="hidden" id="base-recipe-price" name="base-recipe-price" disabled>
-				<h3>Nom de la recette :</h3>
+				<input type="hidden" id="base-name" name="base-name" disabled>
+				<input type="hidden" id="base-image" name="base-image" disabled>
+				<input type="hidden" id="base-price" name="base-price" disabled>
+				<input type="hidden" id="base-type" name="base-type" disabled>
+				<h3 id="label-recipe-name">Nom de la recette :</h3>
 				<input type="text" id="recipe-name" name="recipe-name" placeholder="nom de la recette" required>
 				<h3>Prix :</h3>
 				<input type="number" id="recipe-price" name="recipe-price" max="100" min="0" step="0.05" required><span>€</span>
+				<div id="drink-type-selecter">
+					<h3>Type de boisson :</h3>
+					<input type="radio" id="B_radio" name="drink-type" value="B">
+					<label for="B_radio">Bière</label><br>
+					<input type="radio" id="BSA_radio" name="drink-type" value="BSA">
+					<label for="BSA_radio">Bière sans alcool</label><br>
+					<input type="radio" id="VB_radio" name="drink-type" value="VB">
+					<label for="VB_radio">Vin blanc</label><br>
+					<input type="radio" id="VRos_radio" name="drink-type" value="VRos">
+					<label for="VRos_radio">Vin rosé</label><br>
+					<input type="radio" id="VRou_radio" name="drink-type" value="VRou">
+					<label for="VRou_radio">Vin rouge</label><br>
+					<input type="radio" id="VP_radio" name="drink-type" value="VP">
+					<label for="VP_radio">Vin pichet</label><br>
+					<input type="radio" id="Ci_radio" name="drink-type" value="Ci">
+					<label for="Ci_radio">Cidre</label><br>
+					<input type="radio" id="Ch_radio" name="drink-type" value="Ch">
+					<label for="Ch_radio">Champagne</label><br>
+					<input type="radio" id="AA_radio" name="drink-type" value="AA">
+					<label for="AA_radio">Autre alcool</label><br>
+					<input type="radio" id="SEBSA_radio" name="drink-type" value="SEBSA">
+					<label for="AEBSA_radio">Soda et autre boisson sans alcool</label><br>
+				</div>
 				<h3>Image :</h3>
 				<img id="image_preview" src="../img/plat.png" alt="image du plat">
 				<input type="file" id="file" name="recipe-image" accept="image/png, image/jpeg" hidden required>
@@ -136,19 +169,110 @@ $b = $db->query("SELECT * FROM `boisson`");
 			</div><button class="add_button" onclick="show_add_recipe_pop_up('dessert');">Ajouter</button>
 		</div>
 
-		<div class="recipe_edit">
-			<div id="boisson" class="recipe_list">
+		<div class="drink_edit">
+			<div id="boisson" class="drink_list">
 				<h2>Boisson</h2>
+				<h3>Bière</h3>
 				<ul>
-					<?php while($boisson = $b->fetch()) { ?>
+					<?php while($biere = $b_B->fetch()) { ?>
 					<li>
-						<img src="../<?= $boisson["image"]; ?>">
-						<h3><?= $boisson["name"]; ?> - <?= $boisson["price"] ?>€</h3>
-						<button class="edit_button" onclick="show_edit_recipe_pop_up('boisson', '<?= $boisson["name"]?>', '<?= $boisson["image"] ?>', '<?= $boisson["price"] ?>');">Modifier</button>
+						<img src="../<?= $biere["image"]; ?>">
+						<h3><?= $biere["name"]; ?> - <?= $biere["price"] ?>€</h3>
+						<button class="edit_button" onclick="show_edit_recipe_pop_up('boisson', '<?= $biere["name"]?>', '<?= $biere["image"] ?>', '<?= $biere["price"] ?>', '<?= $biere["type"] ?>');">Modifier</button>
 					</li>
 					<?php } ?>
 				</ul>
-			</div><button class="add_button" onclick="show_add_recipe_pop_up('boisson');">Ajouter</button>
+				<h3>Bière sans alcool</h3>
+				<ul>
+					<?php while($biere_sans_alcool = $b_BSA->fetch()) { ?>
+					<li>
+						<img src="../<?= $biere_sans_alcool["image"]; ?>">
+						<h3><?= $biere_sans_alcool["name"]; ?> - <?= $biere_sans_alcool["price"] ?>€</h3>
+						<button class="edit_button" onclick="show_edit_recipe_pop_up('boisson', '<?= $biere_sans_alcool["name"]?>', '<?= $biere_sans_alcool["image"] ?>', '<?= $biere_sans_alcool["price"] ?>', '<?= $biere_sans_alcool["type"] ?>');">Modifier</button>
+					</li>
+					<?php } ?>
+				</ul>
+				<h3>Vin blanc</h3>
+				<ul>
+					<?php while($vin_blanc = $b_VB->fetch()) { ?>
+					<li>
+						<img src="../<?= $vin_blanc["image"]; ?>">
+						<h3><?= $vin_blanc["name"]; ?> - <?= $vin_blanc["price"] ?>€</h3>
+						<button class="edit_button" onclick="show_edit_recipe_pop_up('boisson', '<?= $vin_blanc["name"]?>', '<?= $vin_blanc["image"] ?>', '<?= $vin_blanc["price"] ?>', '<?= $vin_blanc["type"] ?>');">Modifier</button>
+					</li>
+					<?php } ?>
+				</ul>
+				<h3>Vin rosé</h3>
+				<ul>
+					<?php while($vin_rose = $b_VRos->fetch()) { ?>
+					<li>
+						<img src="../<?= $vin_rose["image"]; ?>">
+						<h3><?= $vin_rose["name"]; ?> - <?= $vin_rose["price"] ?>€</h3>
+						<button class="edit_button" onclick="show_edit_recipe_pop_up('boisson', '<?= $vin_rose["name"]?>', '<?= $vin_rose["image"] ?>', '<?= $vin_rose["price"] ?>', '<?= $vin_rose["type"] ?>');">Modifier</button>
+					</li>
+					<?php } ?>
+				</ul>
+				<h3>Vin rouge</h3>
+				<ul>
+					<?php while($vin_rouge = $b_VRou->fetch()) { ?>
+					<li>
+						<img src="../<?= $vin_rouge["image"]; ?>">
+						<h3><?= $vin_rouge["name"]; ?> - <?= $vin_rouge["price"] ?>€</h3>
+						<button class="edit_button" onclick="show_edit_recipe_pop_up('boisson', '<?= $vin_rouge["name"]?>', '<?= $vin_rouge["image"] ?>', '<?= $vin_rouge["price"] ?>', '<?= $vin_rouge["type"] ?>');">Modifier</button>
+					</li>
+					<?php } ?>
+				</ul>
+				<h3>Vin pichet</h3>
+				<ul>
+					<?php while($vin_pichet = $b_VP->fetch()) { ?>
+					<li>
+						<img src="../<?= $vin_pichet["image"]; ?>">
+						<h3><?= $vin_pichet["name"]; ?> - <?= $vin_pichet["price"] ?>€</h3>
+						<button class="edit_button" onclick="show_edit_recipe_pop_up('boisson', '<?= $vin_pichet["name"]?>', '<?= $vin_pichet["image"] ?>', '<?= $vin_pichet["price"] ?>', '<?= $vin_pichet["type"] ?>');">Modifier</button>
+					</li>
+					<?php } ?>
+				</ul>
+				<h3>Cidre</h3>
+				<ul>
+					<?php while($cidre = $b_Ci->fetch()) { ?>
+					<li>
+						<img src="../<?= $cidre["image"]; ?>">
+						<h3><?= $cidre["name"]; ?> - <?= $cidre["price"] ?>€</h3>
+						<button class="edit_button" onclick="show_edit_recipe_pop_up('boisson', '<?= $cidre["name"]?>', '<?= $cidre["image"] ?>', '<?= $cidre["price"] ?>', '<?= $cidre["type"] ?>');">Modifier</button>
+					</li>
+					<?php } ?>
+				</ul>
+				<h3>Champagne</h3>
+				<ul>
+					<?php while($champagne = $b_Ch->fetch()) { ?>
+					<li>
+						<img src="../<?= $champagne["image"]; ?>">
+						<h3><?= $champagne["name"]; ?> - <?= $champagne["price"] ?>€</h3>
+						<button class="edit_button" onclick="show_edit_recipe_pop_up('boisson', '<?= $champagne["name"]?>', '<?= $champagne["image"] ?>', '<?= $champagne["price"] ?>', '<?= $champagne["type"] ?>');">Modifier</button>
+					</li>
+					<?php } ?>
+				</ul>
+				<h3>Autre alcool</h3>
+				<ul>
+					<?php while($autre_alcool = $b_AA->fetch()) { ?>
+					<li>
+						<img src="../<?= $autre_alcool["image"]; ?>">
+						<h3><?= $autre_alcool["name"]; ?> - <?= $autre_alcool["price"] ?>€</h3>
+						<button class="edit_button" onclick="show_edit_recipe_pop_up('boisson', '<?= $autre_alcool["name"]?>', '<?= $autre_alcool["image"] ?>', '<?= $autre_alcool["price"] ?>', '<?= $autre_alcool["type"] ?>');">Modifier</button>
+					</li>
+					<?php } ?>
+				</ul>
+				<h3>Soda et autre boisson sans alcool</h3>
+				<ul>
+					<?php while($sebsa = $b_SEBSA->fetch()) { ?>
+					<li>
+						<img src="../<?= $sebsa["image"]; ?>">
+						<h3><?= $sebsa["name"]; ?> - <?= $sebsa["price"] ?>€</h3>
+						<button class="edit_button" onclick="show_edit_recipe_pop_up('boisson', '<?= $sebsa["name"]?>', '<?= $sebsa["image"] ?>', '<?= $sebsa["price"] ?>', '<?= $sebsa["type"] ?>');">Modifier</button>
+					</li>
+					<?php } ?>
+				</ul>
+			</div><button class="add_button" onclick="show_add_recipe_pop_up('boisson', true);">Ajouter</button>
 		</div>
 
 		<footer>
@@ -163,10 +287,12 @@ $b = $db->query("SELECT * FROM `boisson`");
 		var pop_up = document.getElementById("recipe_pop_up");
 		var table_input = document.getElementById("table_input");
 		var pop_up_title = document.getElementById("pop-up-title");
+		var label_recipe_name = document.getElementById("label-recipe-name");
 
-		var base_name_input = document.getElementById("base-recipe-name");
-		var base_image_input = document.getElementById("base-recipe-image");
-		var base_price_input = document.getElementById("base-recipe-price");
+		var base_name_input = document.getElementById("base-name");
+		var base_image_input = document.getElementById("base-image");
+		var base_price_input = document.getElementById("base-price");
+		var base_type_input = document.getElementById("base-type");
 
 		var name_input = document.getElementById("recipe-name");
 
@@ -175,6 +301,8 @@ $b = $db->query("SELECT * FROM `boisson`");
 		var button_choose_image = document.getElementById("browse-file-button");
 
 		var price_input = document.getElementById("recipe-price");
+
+		var drink_type_selecter = document.getElementById("drink-type-selecter");
 
 		var div_pop_up_command = document.getElementById("add-pop-up-command");
 
@@ -195,23 +323,31 @@ $b = $db->query("SELECT * FROM `boisson`");
 			}
 		});
 
-		function show_add_recipe_pop_up(table) {
+		function show_add_recipe_pop_up(table, drink = false) {
 			pop_up.style.visibility = "visible";
-			pop_up_title.textContent = "Ajouter une recette :";
 			table_input.value = table;
 			base_name_input.disabled = true;
 			base_image_input.disabled = true;
 			base_price_input.disabled = true;
+			base_type_input.disabled = true;
 			browse_button.setAttribute("required", "");
 			div_pop_up_command.id = "add-pop-up-command";
 			submit_button.setAttribute("name", "add")
 			submit_button.textContent = "Ajouter";
 			sup_button.style.display = "none";
+			if(drink == true) {
+				pop_up_title.textContent = "Ajouter une Boisson :";
+				label_recipe_name.textContent = "Nom de la boisson";
+				drink_type_selecter.style.display = "block";
+			} else {
+				pop_up_title.textContent = "Ajouter une recette :";
+				label_recipe_name.textContent = "Nom de la recette";
+				drink_type_selecter.style.display = "none";
+			}
 		}
 
-		function show_edit_recipe_pop_up(table, name, image, price) {
+		function show_edit_recipe_pop_up(table, name, image, price, type=undefined) {
 			pop_up.style.visibility = "visible";
-			pop_up_title.textContent = "Modifier une recette :";
 			table_input.value = table;
 			base_name_input.disabled = false;
 			base_name_input.value = name;
@@ -223,6 +359,19 @@ $b = $db->query("SELECT * FROM `boisson`");
 			price_input.value = price;
 			browse_button.removeAttribute("required");
 			image_preview.src = "../" + image;
+			if(type != undefined) {
+				pop_up_title.textContent = "Modifier une Boisson :";
+				label_recipe_name.textContent = "Nom de la boisson";
+				base_type_input.disabled = false;
+				base_type_input.value = type;
+				document.getElementById(type + "_radio").checked = true;
+				drink_type_selecter.style.display = "block";
+			} else {
+				pop_up_title.textContent = "Modifier une recette :";
+				label_recipe_name.textContent = "Nom de la recette";
+				base_type_input.disabled = true;
+				drink_type_selecter.style.display = "none";
+			}
 
 			div_pop_up_command.id = "edit-pop-up-command";
 			submit_button.setAttribute("name", "edit");
--- admin/edit_recipe.php
+++ admin/edit_recipe.php
@@ -5,22 +5,22 @@ global $db;
 if(isset($_POST)) {
 	if(isset($_POST["sup"])) {
 
-		unlink("../" . $_POST["base-recipe-image"]);
+		unlink("../" . $_POST["base-image"]);
 
-		$sql = "DELETE FROM " . $_POST["table_input"] . " WHERE name='" . $_POST["base-recipe-name"] . "'";
+		$sql = "DELETE FROM " . $_POST["table_input"] . " WHERE name='" . $_POST["base-name"] . "'";
 		$req = $db->prepare($sql);
 		$req->execute();
 	}
 
 	else if(isset($_POST["edit"])) {
-		if(isset($_POST["recipe-name"]) && !empty($_POST["recipe-name"]) && $_POST["base-recipe-name"] != $_POST["recipe-name"]) {
-			$sql = "UPDATE " . $_POST["table_input"] . " SET `name`=\"" . $_POST["recipe-name"] . "\" WHERE name=\"" . $_POST["base-recipe-name"] . "\"";
+		if(isset($_POST["recipe-name"]) && !empty($_POST["recipe-name"]) && $_POST["base-name"] != $_POST["recipe-name"]) {
+			$sql = "UPDATE " . $_POST["table_input"] . " SET `name`=\"" . $_POST["recipe-name"] . "\" WHERE name=\"" . $_POST["base-name"] . "\"";
 			$req = $db->prepare($sql);
 			$req->execute();
 		}
-		if(isset($_FILES["recipe-image"]["name"]) && !empty($_FILES["recipe-image"]["name"]) && end(explode("/", $_POST["base-recipe-image"])) != $_FILES["recipe-image"]["name"]) {
+		if(isset($_FILES["recipe-image"]["name"]) && !empty($_FILES["recipe-image"]["name"]) && end(explode("/", $_POST["base-image"])) != $_FILES["recipe-image"]["name"]) {
 
-			unlink("../" . $_POST["base-recipe-image"]);
+			unlink("../" . $_POST["base-image"]);
 
 			$file = $_FILES["recipe-image"];
 			$fileName = $_FILES["recipe-image"]["name"];
@@ -31,12 +31,17 @@ if(isset($_POST)) {
 			$fileDest = "../img/recipes/" . $fileName;
 			move_uploaded_file($fileTmpName, $fileDest);
 
-			$sql = "UPDATE " . $_POST["table_input"] . " SET `image`=\"" . end(explode("../", $fileDest)) . "\" WHERE name=\"" . $_POST["base-recipe-name"] . "\"";
+			$sql = "UPDATE " . $_POST["table_input"] . " SET `image`=\"" . end(explode("../", $fileDest)) . "\" WHERE name=\"" . $_POST["base-name"] . "\"";
 			$req = $db->prepare($sql);
 			$req->execute();
 		}
-		if(isset($_POST["recipe-price"]) && !empty($_POST["recipe-price"]) && $_POST["base-recipe-price"] != $_POST["recipe-price"]) {
-			$sql = "UPDATE " . $_POST["table_input"] . " SET `price`=\"" . $_POST["recipe-price"] . "\" WHERE name=\"" . $_POST["base-recipe-name"] . "\"";
+		if(isset($_POST["recipe-price"]) && !empty($_POST["recipe-price"]) && $_POST["base-price"] != $_POST["recipe-price"]) {
+			$sql = "UPDATE " . $_POST["table_input"] . " SET `price`=\"" . $_POST["recipe-price"] . "\" WHERE name=\"" . $_POST["base-name"] . "\"";
+			$req = $db->prepare($sql);
+			$req->execute();
+		}
+		if(isset($_POST["drink-type"]) && $_POST["base-type"] != $_POST["drink-type"]) {
+			$sql = "UPDATE " . $_POST["table_input"] . " SET `type`=\"" . $_POST["drink-type"] . "\" WHERE name=\"" . $_POST["base-name"] . "\"";
 			$req = $db->prepare($sql);
 			$req->execute();
 		}
@@ -52,7 +57,11 @@ if(isset($_POST)) {
 		$fileDest = "../img/recipes/" . $fileName;
 		move_uploaded_file($fileTmpName, $fileDest);
 
-		$sql = "INSERT INTO `" . $_POST['table_input'] . "`(`name`, `image`, `price`) VALUES ('" . $_POST["recipe-name"] . "', '" . explode("../", $fileDest)[1] . "', '" . $_POST["recipe-price"] . "')";
+		if(isset($_POST["drink-type"])) {
+			$sql = "INSERT INTO `" . $_POST['table_input'] . "`(`name`, `image`, `price`, `type`) VALUES ('" . $_POST["recipe-name"] . "', '" . explode("../", $fileDest)[1] . "', '" . $_POST["recipe-price"] . "', '" . $_POST["drink_type"] . "')";
+		} else {
+			$sql = "INSERT INTO `" . $_POST['table_input'] . "`(`name`, `image`, `price`) VALUES ('" . $_POST["recipe-name"] . "', '" . explode("../", $fileDest)[1] . "', '" . $_POST["recipe-price"] . "')";
+		}
 		$req = $db->prepare($sql);
 		$req->execute();
 	}
